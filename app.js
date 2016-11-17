baced13f1384c0c5eaa235da76cb0f35c0295e73
Peetear <Peetear>
2016-11-16 16:40:47 -0800

    inital drag n drop ui working

--- app.js
+++ app.js
@@ -1,20 +1,63 @@
 'use strict';
 (function(){
+
+	console.log('started')
 	function grab(className) {
 		return document.getElementsByClassName(className)[0];
 	}
 
-	function handleDragStart(e) {
-		e.preventDefault();
-		console.log('started drag: '+ e.target.class);
-		e.dataTransfer.setData('text/plain', e.target.class);
-	}
+	document.getElementsByTagName('li')[0].addEventListener('dragstart', function(e){
+		// e.preventDefault();
+		console.log('dragging started')
+		e.dataTransfer.setData('text/plain', e.target.className);
+		e.dataTransfer.dropEffecet = "all"
+		e.dataTransfer.effectAllowed = "all"
 
-	console.log('started')
-
-	document.getElementsByClassName('item-0')[0].addEventListener('dragstart', function(e){
-		e.preventDefault();
-		e.dataTransfer.setData('text/plain', e.target.class);
 	})
 
+	document.getElementsByTagName('li')[1].addEventListener('dragover', function(e){
+		e.preventDefault();
+	})
+
+	document.getElementsByTagName('li')[1].addEventListener('dragover', function(e){
+		e.preventDefault();
+	})
+
+	document.getElementsByTagName('li')[1].addEventListener('drop', function(e){
+		// e.preventDefault();
+		console.log('dropped')
+		var dat = e.dataTransfer.getData("Text");
+		console.log(dat)
+	})
+
+
+
+
+
+
+
+
+	document.getElementsByTagName('li')[1].addEventListener('dragstart', function(e){
+		// e.preventDefault();
+		console.log('dragging started')
+		e.dataTransfer.setData('text/plain', e.target.className);
+		e.dataTransfer.dropEffecet = "all"
+		e.dataTransfer.effectAllowed = "all"
+
+	})
+
+	document.getElementsByTagName('li')[0].addEventListener('dragover', function(e){
+		e.preventDefault();
+	})
+
+	document.getElementsByTagName('li')[0].addEventListener('dragover', function(e){
+		e.preventDefault();
+	})
+
+	document.getElementsByTagName('li')[0].addEventListener('drop', function(e){
+		// e.preventDefault();
+		console.log('dropped')
+		var dat = e.dataTransfer.getData("Text");
+		console.log(dat)
+	})
 })();
